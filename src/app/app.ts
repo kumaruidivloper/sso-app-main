ac5a49e29298ce214d12d84b338e1d5281858f99
kumaruidivloper <kumaruidivloper@gmail.com>
2025-10-02 19:35:33 +1000

    iframe implement

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,12 +6,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSelectChange } from '@angular/material/select';
 import { MatRadioModule } from '@angular/material/radio';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,FormsModule, MatButtonModule, MatSelectModule, MatRadioModule],
+  imports: [CommonModule,FormsModule, MatButtonModule, MatSelectModule, MatRadioModule, MatIconModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
@@ -46,6 +47,7 @@ handleMessage = (event: MessageEvent) => {
   console.log('Message received in App1:', event.data);
   this.message.set(event.data);
   this.conterHandler(event.data.process);
+  this.myIframe.nativeElement.style.height = event.data.iframeSize + 'px'
 };
 
 conterHandler(value: any) {
@@ -58,17 +60,24 @@ conterHandler(value: any) {
 
 incrementApp2(value: any) {
     this.sharedService.sendMessageToApp2(value);
+    this.sharedService.onIframeLoad(value, false)
 }
 
 decrementApp2(value: any) {
   this.sharedService.sendMessageToApp2(value);
+  this.sharedService.onIframeLoad(value, false)
 }
 
 onChange($event:MatSelectChange) {
-      if (!this.sharedService.app2Window || this.sharedService.app2Window.closed) {
+  this.sharedService.updateSeletedOption(this.selectedOption)
+  if(this.selectedOption === '2') {
+     if (!this.sharedService.app2Window || this.sharedService.app2Window.closed) {
       this.sharedService.openApp2();
+    }
+  } else {
+    this.sharedService.onIframeLoad($event.value, true);
   }
-
+ 
 
   setTimeout(() => {
     console.log()
--- src/app/services/shared-service.ts
+++ src/app/services/shared-service.ts
@@ -6,6 +6,7 @@ import { Injectable } from '@angular/core';
 export class SharedService {
     private app2Url = 'http://localhost:4201';
     public app2Window: Window | null = null;
+    private selectedOption: string = '1';
   
     openApp2() {
       // ✅ This will work since it's directly from a user action
@@ -18,12 +19,21 @@ export class SharedService {
           { type: 'GREETING_FROM_APP1', payload: 'Hello from App1!', process: value },
           this.app2Url
         );
-
       } else {
-        console.warn('App2 window not opened yet');
+         this.selectedOption === '1'? '':console.warn('App2 window not opened yet');
       }
     }
 
+  updateSeletedOption(value: string) {
+    this.selectedOption = value;
+  }
 
+
+  onIframeLoad(value: any, resize: boolean) {
+    let data = { type: 'GREETING_FROM_APP1', payload: 'Hello from App1!', process: value, resize: resize }
+    const iframe = document.getElementById('app2-iframe') as HTMLIFrameElement;
+    iframe?.contentWindow?.postMessage(data,
+      'http://localhost:4201');
+  }
 
 }
