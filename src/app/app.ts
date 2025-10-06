3e5449319d26474ab125c1940a4708a90f206a18
kumaruidivloper <kumaruidivloper@gmail.com>
2025-10-06 21:31:30 +1100

    bug fix

--- src/app/app.ts
+++ src/app/app.ts
@@ -47,7 +47,9 @@ handleMessage = (event: MessageEvent) => {
   // if (event.origin  !== 'http://localhost:4201') return;
   console.log('Message received in App1:', event.data);
   this.message.set(event.data);
-  this.conterHandler(event.data.process);
+    if(event.data.iframeSize !== undefined) {
+    this.conterHandler(event.data.process);
+  }
   this.myIframe.nativeElement.style.height = event.data.iframeSize + 'px'
 };
 
